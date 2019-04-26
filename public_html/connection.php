fa6cf2e4474c44e8248657ad68cdaa5efbf9cac5
Sayok Majumder <sayokdeymajumder1998@gmail.com>
2019-04-26 21:48:33 +0530

    Update connection.php

--- public_html/connection.php
+++ public_html/connection.php
@@ -1,10 +1,12 @@
 <?php
 
 define('servername', 'localhost');
-define('user', 'id9391468_admin_chatterbox');
-define('password', 'Mypassword1#');
-define('database', 'id9391468_chatterbox');
+define('user', 'your_db_user_name');
+define('password', 'your_db_user_password');
+define('database', 'your_db_name');
 
 $conn = mysqli_connect(servername, user, password, database) or die("Connection failed: " . $conn->connect_error);
 
-date_default_timezone_set('Asia/Kolkata');
+date_default_timezone_set('Asia/Kolkata');// Change this line acording to your time zone.
+
+//https://www.php.net/manual/en/timezones.php for more details on time zone
